af3db0a2f58c7b81c7e42966aeb416038c002206
farizl <farizl@handsv.com>
2025-01-27 16:35:29 +0700

    add license info api

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "../styles/Home.css";
+import axios from "axios"
 
 const Home = () => {
+    const handleClick = async () => {
+        const res = await axios.get("https://192.168.30.138:443",{
+            headers:{
+                "Content-Type":"application/json",
+                "X-API-KEY":"hid_arcid"
+            }
+        })
+
+        console.log(res)
+
+    }
     return (
     <div className="home">
         <h1>Welcome to demo website</h1>
+        <button onClick={handleClick} className="license">License Info</button>
     </div>)
 }
 
